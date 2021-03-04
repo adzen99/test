29ab7a952c3649f017de5379f3b235d05d3c3acf
Alex Dzen <dzenalex9@gmail.com>
2021-03-04 10:26:35 +0200

    100*100

--- src/Class.java
+++ src/Class.java
@@ -5,10 +5,6 @@ import java.util.function.Consumer;
 public class Class {
 
     public static void main(String[] args){
-        System.out.print("Class 1");
-        System.out.print("Class 2");
-        System.out.print("Class 3");
-        System.out.print("Class 4");
-
+        System.out.print(100*100);
     }
 }
