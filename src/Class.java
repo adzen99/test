2c534df5b30cc08e61a7ddd5cf0fcf8aa96a086d
Alex Dzen <dzenalex9@gmail.com>
2021-03-04 10:30:20 +0200

    abcd

--- src/Class.java
+++ src/Class.java
@@ -5,6 +5,8 @@ import java.util.function.Consumer;
 public class Class {
 
     public static void main(String[] args){
+        System.out.println("abcd");
+        System.out.println("abcd");
         System.out.print(100*100);
     }
 }
